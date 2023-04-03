9bbd01023c89325dcd8ae1d3379e33da88dd7467
Kev <ksayad@gmail.com>
2023-04-03 16:07:13 -0400

    update doc config

--- README.md
+++ README.md
@@ -6,9 +6,21 @@ In this repo, I'm using RenovateBot to perform dependency analysis on specific r
 
 - Create a github PAT/personal access token. I strongly recommend with minimal permissions (repo only) and with a short expiry time
 
-## Setup
+## Setup with NPM
 
 1. We install renovatebot using npm
 2. I then drafted a configuration file 'config.js' based on RenovateBot's configurations documented on https://docs.renovatebot.com/self-hosted-configuration/
 3. The following article was helpful in getting a head start with the config file: https://jerrynsh.com/12-tips-to-self-host-renovate-bot/
-4. When ready, we run renovatebot using the 'renovate' CLI command
+4. To test your config without any real changes, run: `renovate --dry-run=full`
+5. When ready, run renovate: `renovate`
+
+## Setup with the Docker image
+
+1. Install the docker image using podman: `podman pull docker.io/renovate/renovate`
+2. Run (from the same path of this repo):
+```
+$ podman run --rm \
+-v "./config.js:/usr/src/app/config.js" \
+-e RENOVATE_DRY_RUN=full \
+renovate/renovate
+```
--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 module.exports = {
     gitAuthor: 'Renovator <noreply@kevvlvl.renovatebot.org>',
-    token: '',
+    token: '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',
     platform: 'github',
     // executionTimeout: 60, // set max timeout of 60 minutes.
-    // cacheDir: '',         // change where renovate's cache is stored.
     persistRepoData: true,   // repo data is kept between executions. git fetch will be used instead of git clone.
-    dryRun: 'full',       // when activated, no PR is created. Obtain detailed log of what Renovate will do
+    binarySource: 'gradle-wrapper',
     onboardingConfig: {
         extends: ['config:base'],
     },
@@ -14,42 +13,27 @@ module.exports = {
             repository: 'kevvlvl/quarkus-carservice-api',
             cloneSubmodules: true,
             addLabels: 'Renovated by the Renovator!',
+            additionalReviewers:  ["kevvlvl_analyst"],
             packageRules: [
                 {
-                    description: 'Enable Minor and Patch updates types',
-                    matchUpdateTypes: ['minor', 'patch'],
-                    enabled: true,
-                    labels: 'UPDATE-MINOR-PATCH'
+                    description: 'Match Config files',
+                    matchPaths: ["**/*.properties"],
+                    groupName: 'Config files'
                 },
             ],
         },
         {
-            repository: 'kevvlvl/ApiTestCucumber',
+            repository: 'kevvlvl/quarkus-carservice-api',
             cloneSubmodules: true,
             addLabels: 'Renovated by the Renovator!',
+            additionalReviewers:  ["kevvlvl_teamlead"],
             packageRules: [
                 {
-                    description: 'Enable Minor and Patch updates types',
-                    matchUpdateTypes: ['minor', 'patch'],
-                    enabled: true,
-                    labels: 'UPDATE-MINOR-PATCH',
-                    matchPackagePrefixes: [
-                        "com.fasterxml",
-                        "io.rest-assured",
-                        "org.hamcrest",
-                    ],
-                    excludePackagePrefixes: ["io.cucumber", "org.apache"]
-                },
-                {
-                    matchUpdateTypes: ['minor', 'patch'],
-                    enabled: false,
-                    labels: 'UPDATE-MINOR-PATCH',
-                    matchPackagePrefixes: [
-                        "io.cucumber",
-                        "org.apache"
-                    ],
+                    description: 'Match source code',
+                    matchPaths: ["**/*.java"],
+                    groupName: 'Java Source code'
                 },
             ],
-        },
+        }
     ],
 };
